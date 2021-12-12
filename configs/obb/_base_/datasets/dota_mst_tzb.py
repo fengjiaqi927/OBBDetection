49161bae84f4e9fabe2c3b70b30c2e1d27d7ac96
137xiaoyu <wcx90@outlook.com>
2021-12-12 22:47:54 +0800

    added ms-split (mss) distinguished from ms-train (mst)

---
+++ configs/obb/_base_/datasets/dota_mst_tzb.py
@@ -0,0 +1,116 @@
+dataset_type = 'DOTADataset'
+data_root = '/home/wucx/dataset/tzb/input_path_obb/'
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadOBBAnnotations', with_bbox=True,
+         with_label=True, with_poly_as_mask=True),
+    dict(type='LoadDOTASpecialInfo'),
+    dict(type='OBBRandomFlip', h_flip_ratio=0.5, v_flip_ratio=0.5),
+    dict(type='AutoAugment',
+         policies=[
+             [
+                 dict(type='Resize',
+                      img_scale=[(480, 1333), (512, 1333), (544, 1333), (576, 1333),
+                                 (608, 1333), (640, 1333), (672, 1333), (704, 1333),
+                                 (736, 1333), (768, 1333), (800, 1333)],
+                      multiscale_mode='value',
+                      keep_ratio=True)
+             ],
+             [
+                 dict(type='Resize',
+                      img_scale=[(400, 1333), (500, 1333), (600, 1333)],
+                      multiscale_mode='value',
+                      keep_ratio=True),
+                 dict(type='RandomCrop',
+                      crop_type='absolute_range',
+                      crop_size=(384, 600),
+                      allow_negative_crop=True),
+                 dict(type='Resize',
+                      img_scale=[(480, 1333), (512, 1333), (544, 1333),
+                                 (576, 1333), (608, 1333), (640, 1333),
+                                 (672, 1333), (704, 1333), (736, 1333),
+                                 (768, 1333), (800, 1333)],
+                      multiscale_mode='value',
+                      override=True,
+                      keep_ratio=True)
+             ]
+         ]),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='RandomOBBRotate', rotate_after_flip=True,
+         angles=(0, 0), vert_rate=0.5, vert_cls=['roundabout', 'storage-tank']),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DOTASpecialIgnore', ignore_size=2),
+    dict(type='FliterEmpty'),
+    dict(type='Mask2OBB', obb_type='obb'),
+    dict(type='OBBDefaultFormatBundle'),
+    dict(type='OBBCollect', keys=['img', 'gt_bboxes', 'gt_obboxes', 'gt_labels'])
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipRotateAug',
+        img_scale=[(800, 1333)],
+        h_flip=False,
+        v_flip=False,
+        rotate=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='OBBRandomFlip'),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='RandomOBBRotate', rotate_after_flip=True),
+            dict(type='Pad', size_divisor=32),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='OBBCollect', keys=['img']),
+        ])
+]
+
+# does evaluation while training
+# uncomments it  when you need evaluate every epoch
+data = dict(
+    samples_per_gpu=2,
+    workers_per_gpu=2,
+    train=dict(
+        type=dataset_type,
+        task='Task1',
+        classes=('bigship', ),
+        ann_file=data_root + 'train_768/annfiles/',
+        img_prefix=data_root + 'train_768/images/',
+        pipeline=train_pipeline),
+    val=dict(
+        type=dataset_type,
+        task='Task1',
+        classes=('bigship', ),
+        ann_file=data_root + 'val_768/annfiles/',
+        img_prefix=data_root + 'val_768/images/',
+        pipeline=test_pipeline),
+    test=dict(
+        type=dataset_type,
+        task='Task1',
+        classes=('bigship', ),
+        ann_file=data_root + 'val_768/annfiles/',
+        img_prefix=data_root + 'val_768/images/',
+        pipeline=test_pipeline))
+evaluation = dict(metric='mAP')
+
+# disable evluation, only need train and test
+# uncomments it when use trainval as train
+# data = dict(
+#     samples_per_gpu=2,
+#     workers_per_gpu=2,
+#     train=dict(
+#         type=dataset_type,
+#         task='Task1',
+#         classes=('bigship', ),
+#         ann_file=data_root + 'trainval/annfiles/',
+#         img_prefix=data_root + 'trainval/images/',
+#         pipeline=train_pipeline),
+#     test=dict(
+#         type=dataset_type,
+#         task='Task1',
+#         classes=('bigship', ),
+#         ann_file=data_root + 'trainval/annfiles/',
+#         img_prefix=data_root + 'trainval/images/',
+#         pipeline=test_pipeline))
+# evaluation = None
